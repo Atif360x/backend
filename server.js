6ee57c8f7b2ebf64defbdb46eb17f3f21f471ce1
atif mansuri <mansuriatif78@gmail.com>
2026-08-28 17:35:43 +0530

    Refactor server setup and implement notes API in app.js

--- server.js
+++ server.js
@@ -1,19 +1,6 @@
-const express = require('express')
+const app = require('./src/app')
 
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send("hello world")
-})
-
-app.get("/about", (req, res) => {
-    res.send("about page")
-})
-
-app.get("/work", (req, res) => {
-    res.send("work page")
-})
 
 app.listen(3000, () => {
-    console.log("server is listening at port 3000")
+ console.log("server is listening at port 3000")
 })

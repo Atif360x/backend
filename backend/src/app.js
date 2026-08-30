3a8df6f4b1a00d35e8c4f40b485888267ebad22e
atif mansuri <mansuriatif78@gmail.com>
2026-08-30 18:06:10 +0530

    add CORS middleware and update package dependencies

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const multer = require('multer');
 const uploadFile = require('./services/storage.service')
 const postModel = require('./models/post.model')
+const cors = require("cors")
+
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const upload = multer({ storgae: multer.memoryStorage() })
@@ -23,6 +26,7 @@ app.post('/post', upload.single("image"), async (req, res) => {
       })
 
 })
+
 
 app.get('/feed', async (req, res) => {
 
